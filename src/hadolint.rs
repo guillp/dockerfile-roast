9c7df17a328734b3c1fa262088196bbd6df3cf9c
Guillaume Pujol <guillp.dev+github@pm.me>
2026-09-21 17:16:22 +0200

    DF012 is DL3057

--- src/hadolint.rs
+++ src/hadolint.rs
@@ -196,6 +196,7 @@ pub(crate) fn aliases(rule: &str) -> Option<&'static [&'static str]> {
         "DL3046" => Some(&["DF064"]),
         "DL3047" => Some(&["DF056"]),
         "DL3048" => Some(&["DF088"]),
+        "DL3057" => Some(&["DF012"]),
         "DL3059" => Some(&["DF003"]),
         "DL3060" => Some(&["DF055"]),
         "DL3061" => Some(&["DF037"]),
